1fe6d8a603d72cf590a7fb36d703f5d97d9c1250
Ulysses Sousa <ulyssessousa@gmail.com>
2024-03-13 16:55:35 -0300

    Criação dos métodos creditar() e debitar()

--- src/revisao/Conta.java
+++ src/revisao/Conta.java
@@ -5,7 +5,7 @@ public class Conta {
 	private int numero;
 	private double saldo;
 	private static int quantContas = 0;
-	
+	private String mensagemOp;
 	
 	public Conta() {
 		numero = ++quantContas;
@@ -34,6 +34,40 @@ public class Conta {
 
 	public static int getQuantContas() {
 		return quantContas;
+	}
+
+	public String getMensagemOp() {
+		return mensagemOp;
+	}
+
+	public void setMensagemOp(String mensagemOp) {
+		this.mensagemOp = mensagemOp;
+	}
+	
+	public boolean creditar(double valor) {
+		if (valor > 0) {
+			saldo = saldo + valor;
+			mensagemOp = "Crédito realizado com sucesso.";
+			return true;
+		}
+		mensagemOp = "Valor inválido. Crédito não realizado.";
+		return false;
+		
+	}
+	
+	public boolean debitar(double valor) {
+		if (valor <= 0) {
+			mensagemOp = "Valor inválido. Débito não realizado.";
+			return false;
+		}
+		
+		if (valor <= saldo) {
+			saldo -= valor;
+			mensagemOp = "Débito realizado com sucesso.";
+			return true;
+		}
+		mensagemOp = "Saldo insuficiente. Débito não realizado";
+		return false;		
 	}
 	
 	
--- src/revisao/Main.java
+++ src/revisao/Main.java
@@ -1,5 +1,7 @@
 package revisao;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void mostrarQuantContas() {
@@ -16,6 +18,29 @@ public class Main {
 		
 	}
 	
+	public static void realizarCredito(Conta c) {
+		System.out.println("Digite o valor a ser creditado: ");
+		Scanner scan = new Scanner(System.in);
+		double valor = scan.nextDouble();
+		
+		if (c.creditar(valor)) {	
+			System.out.println("Saldo atualizado: " + c.getSaldo());
+		} 
+		System.out.println(c.getMensagemOp());
+	}
+	
+	
+	private static void realizarDebito(Conta c) {
+		System.out.println("Digite o valor a ser debitado: ");
+		Scanner scan = new Scanner(System.in);
+		double valor = scan.nextDouble();
+		
+		if (c.debitar(valor)) {
+			System.out.println("Saldo atualizado: " + c.getSaldo());
+		}
+		System.out.println(c.getMensagemOp());
+	}
+	
 	public static void main(String[] args) {
 		mostrarQuantContas();
 		
@@ -24,7 +49,10 @@ public class Main {
 		
 		mostrarQuantContas();
 		
+		realizarCredito(c1);
 		mostrarInformacoes(c1);
+		
+		realizarDebito(c2);
 		mostrarInformacoes(c2);
 	}
 
